2638931aac4e3999f64a5afd28677b9c07daf716
Ibrahim Wassouf <ib981347@dal.ca>
2023-06-17 02:32:12 -0300

    problem 1 - sum of divisble by 3 or 5
    This is so sad look at all that work just to be wrong...

---
+++ problem_1.hs
@@ -0,0 +1,19 @@
+divisibleByThree = [x | x <- [1..1000], mod x 3 == 0]
+divisibleByFive = [x | x <- [1..1000], mod x 5 == 0]
+allDivisible = divisibleByThree ++ divisibleByFive
+
+sumList :: [Integer] -> Integer
+sumList [] = 0
+sumList (x:xs) = x + sumList xs
+
+combineList :: [Integer] -> [Integer] -> [Integer]
+combineList (x:xs) (y:ys)
+   | x == y              = combineList xs ys
+   | null xs             = ys
+   | null ys             = xs
+   | null xs && null ys  = [] 
+   | otherwise           = x : y : combineList xs ys
+
+
+
+realAnswer = [x | x <- [1..1000], mod x 3 == 0 || mod x 5 == 0]
