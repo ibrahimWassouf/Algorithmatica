1c28f54b8e0aac21dc4d2d584e41bf138d5322b4
Ibrahim Wassouf <ib981347@dal.ca>
2023-06-18 16:19:50 -0300

    even fibonacci numbers sum - Haskell

--- even-fibonacci-numbers.hs
+++ even-fibonacci-numbers.hs
@@ -1,12 +1,17 @@
 list = [1, 1] :: [Integer]
 
-findEven :: [Integer] -> [Integer]
-findEven [] = []
-findEven [x] = [x]
-findEven (x:y:xs) = findEven ((x+y) : xs)
+fibList :: [Integer] -> [Integer]
+fibList [] = []
+fibList (x: []) = [x]
+fibList (x:y:xs)
+ | y >= 4000000    = xs
+ | x >= 4000000    = y:xs
+ | otherwise       = fibList ((x+y) : x : y: xs )
 
 main :: IO ()
 main = do
        let list = [1, 1]
-       let result = findEven list
-       print result 
+       let result = fibList list
+       let getEven = [x | x <- result, even x]
+       print getEven
+       print (sum getEven)
