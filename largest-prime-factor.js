5e20358015442081ee665f8cefc97a312df83a23
Ibrahim Wassouf <ib981347@dal.ca>
2023-06-20 19:41:19 -0300

    Modified find largest prime algorithm - Javascript
    I missed a while loop and so it didn't work for even numbers.
    I let into impulses and looked it up online, but I made up for
    it somewhat by using my own implementation.

--- largest-prime-factor.js
+++ largest-prime-factor.js
@@ -1,8 +1,9 @@
 import { argv } from "process";
 
 let composite = parseInt(argv[2]);
+let sqrt = Math.ceil(Math.sqrt(composite));
 let currPrime = 2;
-
+/*
 while (composite != currPrime) {
   if (composite % currPrime == 0) {
     console.log(currPrime);
@@ -12,3 +13,16 @@ while (composite != currPrime) {
 }
 
 console.log(currPrime);
+*/
+// Take two
+let divisibles = [];
+console.log(sqrt);
+for (let i = 1; i <= sqrt; i++) {
+  if (composite % i == 0) {
+    divisibles.push(i);
+  }
+}
+console.log(divisibles);
+let primes = divisibles.filter((num) => Math.pow(2, num) % num == 2 % num);
+
+console.log(primes);
