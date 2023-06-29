cb4a97c21d1f03689f196a60897469f0ac17b3cf
Ibrahim Wassouf <ib981347@dal.ca>
2023-06-28 22:48:26 -0300

    valid parentheses - Haskell

---
+++ valid-parentheses.hs
@@ -0,0 +1,18 @@
+valid :: [Char] -> [Char] -> Bool
+valid [] [] = True
+valid [x] [] = False
+valid [] (y:ys) = False
+valid (x:z:xs) []
+ | x == '('   = valid (z:xs) (x:[])
+ | x == '{'   = valid (z:xs) (x:[])
+ | x == '['   = valid (z:xs) (x:[])
+valid (x:xs) (y:ys)
+ | x == '('   = valid (xs) (x:y:ys)
+ | x == '{'   = valid (xs) (x:y:ys)
+ | x == '['   = valid xs (x:y:ys)
+ | x == ')' && y == '('   = valid xs ys
+ | x == ')' && y /= '('   = False
+ | x == '}' && y == '{'   = valid xs ys
+ | x == '}' && y /= '{'   = False
+ | x == ']' && y == '['   = valid xs ys
+ | otherwise              = False
